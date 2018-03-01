59e0bddad5a980b043c28d0c627d9538f370b0a5
arildlil <arildlil@student.matnat.uio.no>
2018-03-01 18:54:29 +0100

    feat Added unit tests to test the new 'Surface_init' and 'Surface_move' functions

--- tests.c
+++ tests.c
@@ -2,6 +2,7 @@
 #include "utils.h"
 
 #include "curses.h"
+#include <curses.h>
 
 
 
@@ -14,6 +15,36 @@ static void test_Curses_init(void **state) {
     assert_int_equal(Curses_exit(), TRUE);
 }
 
+static void test_Surface_init(void **state) {
+    Surface surfaces[2];
+    Surface_init(&surfaces[0], 0, 0, "", NULL);
+    assert_int_equal(surfaces[0].x, 0);
+    assert_int_equal(surfaces[0].y, 0);
+    assert_int_equal(surfaces[0].content, "");
+    assert_int_equal(surfaces[0].bounce, FALSE);
+
+    Surface_options options = {.bounce = TRUE};
+    Surface_init(&surfaces[1], 10, 10, "Hello", &options);
+    assert_int_equal(surfaces[1].x, 10);
+    assert_int_equal(surfaces[1].y, 10);
+    assert_int_equal(surfaces[1].content, "Hello");
+    assert_int_equal(surfaces[1].bounce, TRUE);
+}
+
+static void test_Surface_move(void **state) {
+    Surface surfaces[2]; 
+    Surface_init(&surfaces[0], 5, 2, "o", NULL);
+    Surface_move(&surfaces[0], 5, 5);
+    assert_int_equal(surfaces[0].x, 10);
+    assert_int_equal(surfaces[0].y, 7);
+    
+    Surface_options options = {.bounce = TRUE};
+    Surface_init(&surfaces[1], 30, 30, "o", &options);
+    Surface_move(&surfaces[1], max_x+1, max_y+1);
+    assert_int_equal(surfaces[1].x, max_x);
+    assert_int_equal(surfaces[1].y, max_y);
+}
+
 
 
 /* ----- | Other | ------ */
@@ -23,9 +54,12 @@ static void null_test_success(void **state) {
 }
 
 int main(void) {
+
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(null_test_success),
         cmocka_unit_test(test_Curses_init),
+        cmocka_unit_test(test_Surface_init),
+        cmocka_unit_test(test_Surface_move),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
