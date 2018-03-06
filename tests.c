d35a768a2e76360ce36507b33869abf3260707b6
arildlil <arildlil@student.matnat.uio.no>
2018-03-06 17:38:05 +0100

    test Created tests for the new GameObject type

--- tests.c
+++ tests.c
@@ -223,6 +223,37 @@ static void test_Units_constructors(void **state) {
     //GameObject_init(&objects[0], SURFACE_ARCHER, 0, 0);
 }
 
+static void test_Gameobject(void **state) {
+    (void)state;
+
+    GameObject object;
+    GameObject_init(&object, 3, 5, &UNIT_IMAGE_ARCHER);
+    assert_int_equal(object.is_active(&object), TRUE);
+
+    assert_int_equal(object.get_x(&object), 3);
+    assert_int_equal(object.get_y(&object), 5);
+    object.movement(&object, 2, 3);
+    assert_int_equal(object.get_x(&object), 5);
+    assert_int_equal(object.get_y(&object), 8);
+    object.set_x(&object, 2);
+    object.set_y(&object, 1);
+    assert_int_equal(object.get_x(&object), 2);
+    assert_int_equal(object.get_y(&object), 1);
+    object.set_xy(&object, 3, 4);
+    assert_int_equal(object.get_x(&object), 3);
+    assert_int_equal(object.get_y(&object), 4);
+
+    assert_memory_equal(object.get_image(&object), &UNIT_IMAGE_ARCHER, sizeof(Image));
+    assert_int_equal(object.get_width(&object), UNIT_IMAGE_ARCHER.get_width(&UNIT_IMAGE_ARCHER));
+    assert_int_equal(object.get_height(&object), UNIT_IMAGE_ARCHER.get_height(&UNIT_IMAGE_ARCHER));
+    int image_width = object.get_width(&object);
+    char **pixels = object.get_pixels(&object);
+    char **pixels_original = UNIT_IMAGE_ARCHER.get_pixels(&UNIT_IMAGE_ARCHER);
+    assert_memory_equal(pixels[0], pixels_original[0], sizeof(char) * image_width);
+    object.free(&object);
+    assert_int_equal(object.is_active(&object), FALSE);
+}
+
 
 /* ----- | Other | ------ */
 
@@ -246,6 +277,8 @@ int main(void) {
         cmocka_unit_test(test_unit_images),
         cmocka_unit_test(test_unit_surfaces),
         cmocka_unit_test(test_Units_constructors),
+
+        cmocka_unit_test(test_Gameobject),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
