debf466af0e16ade82a38a953c17d423751a8d8b
arildlil <arildlil@student.matnat.uio.no>
2018-03-06 13:42:24 +0100

    feat Started work on the new 'unit_surfaces' files and added them to the Makefile

---
+++ unit_surfaces.c
@@ -0,0 +1,17 @@
+#include "unit_surfaces.h"
+
+
+
+static boolean inited = FALSE;
+
+Surface* UnitSurface_archer(Surface *surface, int x, int y) {
+    return surface;
+}
+
+Surface* UnitSurface_swordman(Surface *surface, int x, int y) {
+    return surface;
+}
+
+Surface* UnitSurface_spearman(Surface *surface, int x, int y) {
+    return surface;
+}
---
+++ unit_surfaces.h
@@ -0,0 +1,18 @@
+#ifndef UNIT_SURFACES_H
+#define UNIT_SURFACES_H
+
+#include "unit_images.h"
+
+/* (All functions above)
+ *
+ * Fills out the surface object to create a unit.
+ * @arg surface: The surface object to fill out.
+ * @arg x: The x-coordinate to place it.
+ * @arg y: The y-coordinate to place it.
+ * @return: The object that just got initialized.
+ */
+Surface* UnitSurface_archer(Surface *surface, int x, int y);
+Surface* UnitSurface_swordman(Surface *surface, int x, int y);
+Surface* UnitSurface_spearman(Surface *surface, int x, int y);
+
+#endif
