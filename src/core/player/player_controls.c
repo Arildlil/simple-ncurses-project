29657b0a437637c2ad5eb9c33e95b5c3ee1dc154
Arild <arildlil@protonmail.com>
2020-08-30 12:59:13 +0200

    change Changed the movement system again
    Press 'c' to continue moving in the same direction as
    on the previous key press.

--- src/core/player/player_controls.c
+++ src/core/player/player_controls.c
@@ -8,6 +8,7 @@
 
 
 static void set_movement_x_y(GameObject *hero, int x, int y);
+static void set_autorun(GameObject *hero);
 
 
 
@@ -22,6 +23,7 @@ enum {
     MOVE_UP_RIGHT = 'e',
     MOVE_DOWN_LEFT = '<',
     MOVE_DOWN_RIGHT = 'x',
+    AUTORUN = 'c',
 
     STOP = 'z',
     ATTACK = ' ',
@@ -73,6 +75,9 @@ boolean PlayerControls_handle_input_char(char input, GameObject *hero) {
             break;
         case MOVE_DOWN_RIGHT:
             set_movement_x_y(hero, 1, 1);
+        case AUTORUN:
+            set_autorun(hero);
+            hero->m->movement(hero, per_tick_movement_x, per_tick_movement_y);
             break;
         case STOP:
             set_movement_x_y(hero, 0, 0);
@@ -94,17 +99,19 @@ static void set_movement_x_y(GameObject *hero, int x, int y) {
     assert(x <= 1 && x >= -1);
     assert(y <= 1 && y >= -1);
     
-    if (x == last_movement_x && y == last_movement_y) {
-        per_tick_movement_x = x;
-        per_tick_movement_y = y;
-        hero->m->movement(hero, per_tick_movement_x, per_tick_movement_y);
-    } else {
-        per_tick_movement_x = 0;
-        per_tick_movement_y = 0;
-        hero->m->movement(hero, x, y);        
-    }
+    hero->m->movement(hero, x, y);        
     
     last_movement_x = x;
     last_movement_y = y;
+    
+    per_tick_movement_x = 0;
+    per_tick_movement_y = 0;
+}
+
+static void set_autorun(GameObject *hero) {
+    assert(hero);
+    
+    per_tick_movement_x = last_movement_x;
+    per_tick_movement_y = last_movement_y;
 }
 
